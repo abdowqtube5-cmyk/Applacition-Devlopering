e324d546eff5ac765a31a8b7c3d82b8aeb146be0
Abdow Q <abdowqtube5@gmail.com>
2025-12-20 20:05:06 +0300

    one

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginPro());
+}
+
+class LoginPro extends StatelessWidget {
+  const LoginPro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
